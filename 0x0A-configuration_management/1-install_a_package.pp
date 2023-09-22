f97e7fed290a39a78ac1b219ae15adbc4d4912cb
moulineE <elmahdi.mouline@live.fr>
2023-09-22 17:57:49 +0100

    Task 1. Install a package FiX formating

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,11 @@
 # sing Puppet, install flask from pip3.
 
 exec { 'apt-get update':
-    command => '/usr/bin/apt-get update'
+    command => '/usr/bin/apt-get update',
 }
 
 package { 'flask':
-    ensure   => '2.1.0'
-    require  => Exec['apt-get update']
-    provider => 'pip3'
+    ensure   => '2.1.0',
+    require  => Exec['apt-get update'],
+    provider => 'pip3',
 }
