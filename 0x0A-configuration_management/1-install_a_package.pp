ac3003317a4982aa66c0d6bb738d4a1260be3334
moulineE <elmahdi.mouline@live.fr>
2023-09-22 17:47:28 +0100

    Task 1. Install a package

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,10 @@
+exec { 'apt-get update':
+    command => '/usr/bin/apt-get update'
+}
+
+package { 'flask':
+    ensure   => '2.1.0'
+    require  => Exec['apt-get update']
+    provider => 'pip3'
+}
+
