7f8b7d527fb832dc356d5594287088b664c3cf86
moulineE <elmahdi.mouline@live.fr>
2023-09-22 18:09:56 +0100

    Task 2. Execute a command FIx: lint formating

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # Using Puppet, create a manifest that kills a process named killmenow.
 
 exec { 'pkill':
-    command => '/usr/bin/pkill killmenow', 
+    command => '/usr/bin/pkill killmenow',
 }
