5d1ee3496e75a5156c285cd49e6ac17e681e2b3b
moulineE <elmahdi.mouline@live.fr>
2023-11-07 16:43:52 +0100

    Task 1. Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+a function that queries the Reddit API and returns the number of subscribers
+for a given subreddit. If an invalid subreddit is given,
+the function should return 0.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    a function that queries the Reddit API and prints
+    the titles of the first 10 hot posts listed for a given subreddit
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(url)
+    if response.status_code == 200:
+        json = response.json()
+        for i in range(10):
+            print(json.get('data').get('children')[i].get('data').get('title'))
+    else:
+        print('None')
