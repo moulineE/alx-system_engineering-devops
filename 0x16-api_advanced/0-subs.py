a9a6cd6c3161632b7ea8cfe652225dbd5594d9be
moulineE <elmahdi.mouline@live.fr>
2023-11-08 15:08:06 +0100

    Task 0 & 1. FIX: add  User-Agent and no redirects

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,10 @@ def number_of_subscribers(subreddit):
     function that queries the Reddit API and returns the number of subscribers
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url)
+    response = requests.get(url,
+                            headers={"User-Agent": "Mozilla/5.0"},
+                            allow_redirects=False,
+                            )
     if response.status_code == 200:
         json = response.json()
         return json.get('data').get('subscribers')
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -13,7 +13,10 @@ def top_ten(subreddit):
     the titles of the first 10 hot posts listed for a given subreddit
     """
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    response = requests.get(url)
+    response = requests.get(url,
+                            headers={"User-Agent": "Mozilla/5.0"},
+                            allow_redirects=False,
+                            )
     if response.status_code == 200:
         json = response.json()
         for i in range(10):
